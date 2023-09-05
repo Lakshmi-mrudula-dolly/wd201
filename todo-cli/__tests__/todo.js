286f871204dbdc1356fa84b611a7851d4a03a3f4
lakshmimrudula <puttalakshmimrudula@gmail.com>
2023-09-05 18:48:32 +0530

    changes

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,79 +1,67 @@
 /* eslint-disable no-undef */
-const todoList = require('../todo');
-const { default: expect } = require("expect");
-const {
-    all,
-    markAsComplete,
-    add,
-    overdue,     // Import overdue function
-    dueToday,    // Import dueToday function
-    dueLater     // Import dueLater function
-} = todoList();
- 
-describe("Todolist Test Suite", () => {
-    beforeAll(() => {
-        const today = new Date();
-        const tomorrow = new Date(new Date().setDate(today.getDate() + 1));
-        expect(all.length)
-.toBe(0);
-        add({
-            title: "File taxes",
-            dueDate: tomorrow.toISOString().slice(0,10),
-            completed:false,
-        });
-    expect(all.length).toBe(1);
-    });
-    test("Should add new todo", () => {
-        const todoit = all.length;
-      //  expect(all.length).toBe(todoit+1);
-        add({
-            title: 'Task 1',
-            dueDate: new Date().toISOString().slice(0, 10), // Due today (not overdue)
-            completed: false,
-        });
-        expect(all.length).toBe(todoit+1);
-    });
+const {default :expect}=require("expect");
+const todoList = require("../todo");
+const {all,markAsComplete,add,overdue,dueToday,dueLater} = todoList();
 
-    test("Should mark a today as completed", () => {
+describe("TodoList Test Suite",() =>{
+    beforeAll(() =>{
+        const today=new Date();
+        const tomorrow = new Date(new Date().setDate(today.getDate()+1));
+        expect(all.length).toBe(0);
+        add(
+            {
+                title:"File taxes",
+                dueDate:tomorrow.toISOString().slice(0,10),
+                completed: false,
+            }
+        );
+        expect(all.length).toBe(1);
+    })
+    test("Should add new todo", () =>{
+        const todoItemsCount=all.length;
+        add({
+                title:"Service Vehicle",
+                dueDate:new Date().toISOString().slice(0,10),
+                completed: false,
+            });
+        expect(all.length).toBe(todoItemsCount+1);
+    });
+    test("Should mark a todo as complete",() => {
         expect(all[0].completed).toBe(false);
         markAsComplete(0);
         expect(all[0].completed).toBe(true);
     });
-
-    test("retrieval of overdue items", () => {
-        const today = new Date();
-        const yesterday = new Date(new Date().setDate(today.getDate() - 1));
-        const overdueCount = overdue().length;
+    test("Should retrive overdue items", () =>{
+        const today=new Date();
+        const yesterday = new Date(new Date().setDate(today.getDate()-1));
+        const overdueCount=overdue().length;
         add({
-            title: "assignment",
-            dueDate: yesterday.toISOString().slice(0, 10),
-            completed: false,
-          });
+                title:"Submit assignment",
+                dueDate:yesterday.toISOString().slice(0,10),
+                completed: false,
+            });
         expect(overdue().length).toBe(overdueCount+1);
     });
-
-    test("retrieval of due today items", () => {
-
-        const today = new Date();
-    const dueTodayCount = dueToday().length;
-    add({
-      title: "rent",
-      dueDate: today.toISOString().slice(0, 10),
-      completed: true,
-    });
-    expect(dueToday().length).toBe(dueTodayCount + 1);
-    });
-
-    test("retrieval of due later items", () => {
-    
-        const today = new Date();
-        const tomorrow = new Date(new Date().setDate(today.getDate() + 1));
-        const dueLatreCount = dueLater().length;
+    test("Should retrive due today items", () =>{
+        const today=new Date();
+        const dueTodayCount=dueToday().length;
         add({
-          title: "Pay bill",
-          dueDate: tomorrow.toISOString().slice(0, 10),
-          completed: false,
-        });
-        expect(dueLater().length).toBe(dueLatreCount + 1);
+                title:"Pay rent",
+                dueDate:today.toISOString().slice(0,10),
+                completed: true,
+            });
+        expect(dueToday().length).toBe(dueTodayCount+1);
+    });
+    test("Should retrive due later items", () =>{
+        const today=new Date();
+        const tomorrow = new Date(new Date().setDate(today.getDate()+1));
+        const duelaterCount=dueLater().length;
+        add({
+                title:"Pay electric bill",
+                dueDate:tomorrow.toISOString().slice(0,10),
+                completed: false,
+            });
+        expect(dueLater().length).toBe(duelaterCount+1);
+        console.log(all);
     });
 });
